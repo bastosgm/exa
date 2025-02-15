f0b35e46a571a198a63527a03561f282072dd917
Gustavo Bastos <bastosgm@outlook.com>
2025-02-15 16:58:04 -0300

    chore(App): add simple log when the app starts

--- src/main/java/com/exa/MyApp.java
+++ src/main/java/com/exa/MyApp.java
@@ -2,10 +2,14 @@ package com.exa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication(scanBasePackages = "com.exa")
 public class MyApp {
+    private static final Logger logger = LoggerFactory.getLogger(MyApp.class);
     public static void main(String[] args) {
+        logger.info("Starting application at PORT 8080");
         SpringApplication.run(MyApp.class, args);
     }
 }
