2fb6e1cb3db1c63cbfd230daae3bc7640cf79d71
Gustavo Bastos <bastosgm@outlook.com>
2025-02-17 20:48:41 -0300

    style: remove unecessary lib

--- src/main/java/com/exa/services/UserService.java
+++ src/main/java/com/exa/services/UserService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
